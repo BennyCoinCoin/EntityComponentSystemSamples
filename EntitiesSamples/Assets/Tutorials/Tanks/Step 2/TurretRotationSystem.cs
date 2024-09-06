4aaf510e21f0777264c3de90a1ec64e7c045ffef
BennyCoinCoin <benjamin.delefosse@gmail.com>
2024-09-06 18:39:29 +0200

    Update TurretRotationSystem.cs
    Edited the 1st comment to reflect the fact the [BurstCompile] attribute doesn't need to be added to the struct anymore (it might have been confusing to read the previous comment and not seeing the attribute in the latest script)

--- EntitiesSamples/Assets/Tutorials/Tanks/Step 2/TurretRotationSystem.cs
+++ EntitiesSamples/Assets/Tutorials/Tanks/Step 2/TurretRotationSystem.cs
@@ -7,8 +7,7 @@ namespace Tutorials.Tanks.Step2
 {
     // Unmanaged systems based on ISystem can be Burst compiled, but this is not yet the default,
     // so we have to explicitly opt into Burst compilation with the [BurstCompile] attribute.
-    // It has to be added on BOTH the struct AND the OnCreate/OnDestroy/OnUpdate functions to be
-    // effective.
+    // It has to be added on the OnCreate/OnDestroy/OnUpdate functions to be effective.
     public partial struct TurretRotationSystem : ISystem
     {
         [BurstCompile]
